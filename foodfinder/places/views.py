f4af7591892bfa82d79c5ff1ea1febb4eabf770d
eric lu <eric.lu32712@gmail.com>
2024-10-01 20:53:14 -0400

    Reverting to the state of the project

--- foodfinder/places/views.py
+++ foodfinder/places/views.py
@@ -175,19 +175,3 @@ def signup(request):
     else:
         form = SignUpForm()
     return render(request, 'registration/signup.html', {'form': form})
-
-#added favorite
-@login_required
-def add_to_favorite(request):
-    if request.method == 'POST':
-        restaurant_id = request.POST.get('restaurant_id')
-        food_place = get_object_or_404(FoodPlace, pk=restaurant_id)
-
-        favorite, created = Favorite.objects.get_or_create(user=request.user, food_place=food_place)
-
-        if created:
-            return JsonResponse({'status': 'success', 'message': 'Added to favorites!'})
-        else:
-            return JsonResponse({'status': 'exists', 'message': 'Already in favorites.'})
-
-    return JsonResponse({'status': 'error', 'message': 'Invalid request.'})
